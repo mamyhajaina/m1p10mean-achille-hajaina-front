f8ea2bb302c06d50c558b0359bbbe28aa4c33b3a
Achille Aubin <achilleaubinfanomezantsoa@gmail.com>
2024-02-09 17:51:51 +0100

    context path

--- src/environments/environments.ts
+++ src/environments/environments.ts
@@ -1,6 +1,5 @@
 export const environments = {
-    production: false,
-    BASE_URL: 'http://localhost:8999',
-    CONTEXT_PATH: '/salonBeauto',
-    url_image: '/assets/assets/img/'
-}
+  production: false,
+  BASE_URL: 'http://localhost:8999',
+  url_image: '/assets/assets/img/',
+};
