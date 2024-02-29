c8fb358c9331021b68e52305582cfa21e8bdad18
Achille Aubin <achilleaubinfanomezantsoa@gmail.com>
2024-02-29 10:03:48 +0100

    f

--- src/environments/environments.ts
+++ src/environments/environments.ts
@@ -1,5 +1,5 @@
 export const environments = {
   production: false,
-  BASE_URL: 'http://localhost:1200',
+  BASE_URL: 'https://charmeeclatservice.onrender.com',
   url_image: '/assets/assets/img/',
 };
